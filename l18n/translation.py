7baeca6dd0f87709f384a9cd5960a27feeae7f12
Olivier Le Moign <olemoign@parsys.com>
2020-10-28 14:40:33 +0100

    MutableMapping compat.

--- l18n/translation.py
+++ l18n/translation.py
@@ -2,10 +2,11 @@ import os
 import gettext
 import bisect
 from locale import getdefaultlocale
-from collections.abc import MutableMapping
 from copy import copy, deepcopy
 
 import six
+
+from .compat import MutableMapping
 
 
 class Trans(object):
